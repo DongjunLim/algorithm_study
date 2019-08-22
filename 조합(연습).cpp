5da3739b976d725c403bb6e2e4c79a4f09061dbc
DongjunLim <40556417+DongjunLim@users.noreply.github.com>
2019-08-22 20:01:14 +0900

    08_22

---
+++ 조합(연습).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int c(int n, int k) {
+	if (k == 0 || n == k)	return 1;
+	return c(n - 1, k - 1) + c(n - 1, k);
+
+}
+
+int main()
+{
+	int num = c(10, 3);
+	cout << "result: " << num;
+	return 0;
+
+}
